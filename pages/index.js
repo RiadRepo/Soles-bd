9266d2ed1be64a8b109553efefd88f657cf9c100
RiadRepo <abriad1835@gmail.com>
2023-09-21 02:02:01 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <div className="h-[800px] mx-auto w-full max-w-screen-xl px-5">
+        Working
+      </div>
+      <Footer />
+    </main>
+  );
+}
